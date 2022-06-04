d40af1431a9d37742dd32a48a82adb1f8300d4d7
thorvalld <anis.bejaoui@esprit.tn>
2022-06-04 12:35:02 +0100

    initial commit

---
+++ lib/ui/pages/shifts_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ShiftScreen extends StatefulWidget {
+  const ShiftScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShiftScreen> createState() => _ShiftScreenState();
+}
+
+class _ShiftScreenState extends State<ShiftScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
